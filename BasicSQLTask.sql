0a55b2ecdcca180135a31a09f2735831e1cf1c52
Bhanu Kiran <bhanukiran.g@keka.com>
2026-01-19 11:25:59 +0530

    Remove bullet point

--- BasicSQLTask.sql
+++ BasicSQLTask.sql
@@ -242,27 +242,27 @@ GROUP BY Title
 HAVING SUM(Age) > 30;
 
 -- Basic Data Modification
---  •	Lisa Ray just got married to Michael Moore. She has requested that her last name be updated to Moore. 
+-- Lisa Ray just got married to Michael Moore. She has requested that her last name be updated to Moore. 
 UPDATE Employee
 SET LastName = 'Moore'
 WHERE FirstName = 'Lisa' AND LastName = 'Ray';
 
--- •	Ginger Finger's birthday is today, add 1 to his age and a bonus of 5000
+-- Ginger Finger's birthday is today, add 1 to his age and a bonus of 5000
 UPDATE Employee
 SET Age = Age + 1, Salary = Salary + 5000
 WHERE FirstName = 'Ginger' AND LastName = 'Finger';
 
--- •	All 'Programmer's are now called "Engineer"s. Update all titles accordingly. 
+-- All 'Programmer's are now called "Engineer"s. Update all titles accordingly. 
 UPDATE Employee
 SET Title = 'Engineer'
 WHERE Title = 'Programmer';
 
--- •	Everyone whose making under 30000 are to receive a 3500 bonus. 
+-- Everyone whose making under 30000 are to receive a 3500 bonus. 
 UPDATE Employee
 SET Salary = Salary + 3500
 WHERE Salary < 30000;
 
--- •	Everyone whose making over 35500 are to be deducted 15% of their salaries
+-- Everyone whose making over 35500 are to be deducted 15% of their salaries
 UPDATE Employee
 SET Salary = Salary * 0.85
 WHERE Salary > 35500;
